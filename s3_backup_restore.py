c9bcaec11e854eb4b92230d819488eac42869d4a
Daniel Thielking <daniel@thielking.cloud>
2018-04-25 16:41:46 +0200

    Added S3BackupRestore class with threading possibility. Changed parameter --thread-count. Removed dummy functions. Restructured execution path.

--- s3_backup_restore.py
+++ s3_backup_restore.py
@@ -6,6 +6,8 @@ import sys
 import threading
 from datetime import datetime
 
+copy_queue = queue.Queue()
+
 logging.basicConfig(
     level=logging.ERROR,
     format="%(asctime)s [%(levelname)s] %(funcName)s line %(lineno)d: %(message)s"
@@ -18,13 +20,47 @@ parser.add_argument('--source-bucket', '-s', help='Source Bucket to sync diff fr
 parser.add_argument('--destination-bucket', '-d', help='Destination Bucket to sync to.', required=True)
 parser.add_argument('--prefix', default='', help='Prefix to list objects from and to sync to.')
 parser.add_argument('--tag-deleted', action='store_true', help='Tag all objects that are deleted in source bucket but still present in backup bucket.')
-parser.add_argument('--threads-count', default=100, help='Starting count for threads.')
+parser.add_argument('--thread-count', default=10, metavar='N', type=int, help='Starting count for threads.')
 parser.add_argument('--verbose', '-v', action='count')
 parser.add_argument('--dry-run', action='store_true', help='Simulate what will happen.')
 cmd_args = parser.parse_args()
 
 if cmd_args.verbose and cmd_args.verbose > 0:
     logger.setLevel(logging.DEBUG)
+
+
+class S3GetDifferentKeys(threading.Thread):
+
+
+
+class S3BackupRestore(threading.Thread):
+    def __init__(self, thread_number, aws_session, source_bucket, destination_bucket):
+        threading.Thread.__init__(self)
+        self.thread_number = thread_number
+        self.aws_session = aws_session
+        self.source_bucket = source_bucket
+        self.destination_bucket = destination_bucket
+
+    def run(self):
+        global copy_queue
+        self.resource = self.aws_session.resource('s3')
+        self.dest_bucket = self.resource.Bucket(self.destination_bucket)
+
+        while not copy_queue.empty():
+            self.key = copy_queue.get()
+            self.dest_obj = self.dest_bucket.Object(self.key)
+
+            logger.info("Thread number {} copying key: {}".format(self.thread_number, self.key))
+            try:
+                self.dest_obj.copy(
+                    {
+                        'Bucket': self.source_bucket,
+                        'Key': self.key
+                    }
+                )
+                copy_queue.task_done()
+            except ConnectionRefusedError as exc:
+                logger.error("To many connections open.")
 
 
 def get_s3_keys(s3_client, bucket, prefix=''):
@@ -101,64 +137,47 @@ def tag_deleted_keys(s3_client, backup_bucket, keys_to_tag):
     except:
         logger.exception("")
 
-
-def backup_keys(s3_client, source_bucket, destination_bucket, prefix=""):
-    pass
-
-
-def restore_keys(s3_client, source_bucket, destination_bucket, prefix=""):
-    pass
-
-
 if __name__ == '__main__':
     try:
         if cmd_args.profile:
-            session = boto3.session.Session(profile_name=cmd_args.profile)
+            aws_session = boto3.session.Session(profile_name=cmd_args.profile)
         else:
-            session = boto3.session.Session()
-        s3_client = session.client('s3')
+            aws_session = boto3.session.Session()
+        s3_client = aws_session.client('s3')
     except:
         logger.exception("")
         sys.exit(127)
     else:
+        # Getting S3 keys from source bucket
+        logging.info("List S3 Keys from {}".format(cmd_args.source_bucket))
+        source_bucket_keys = get_s3_keys(s3_client, cmd_args.source_bucket, cmd_args.prefix)
+        logger.debug("Keys from {} bucket:\n{}".format(cmd_args.source_bucket, source_bucket_keys))
+
+        # Getting S3 keys from source bucket
+        logging.info("List S3 Keys from {}".format(cmd_args.destination_bucket))
+        dest_bucket_keys = get_s3_keys(s3_client, cmd_args.destination_bucket, cmd_args.prefix)
+        logger.debug("Keys from {} bucket:\n{}".format(cmd_args.destination_bucket, dest_bucket_keys))
+
+        # Adding source bucket keys to queue to make it thread safe
+        for key in source_bucket_keys:
+            copy_queue.put(key)
+
+        th = list()
+        logger.info("Generating {} threads.".format(cmd_args.thread_count))
+        for i in range(0, cmd_args.thread_count):
+            th.append(S3BackupRestore(i, aws_session, cmd_args.source_bucket, cmd_args.destination_bucket))
+            th[i].daemon = True
+            th[i].start()
+        copy_queue.join()
+
+        for i in range(0, cmd_args.thread_count):
+            th[i].join()
+
         # If script will be calle with flag --tag-deleted we will tag all newly deleted files
         # from source bucket as deleted in backup bucket
         if cmd_args.tag_deleted:
-            logging.info("List S3 Keys from {}".format(cmd_args.source_bucket))
-            source_bucket_keys = get_s3_keys(s3_client, cmd_args.source_bucket, cmd_args.prefix)
-            logger.debug("Keys from {} bucket:\n{}".format(cmd_args.source_bucket, source_bucket_keys))
-            logging.info("List S3 Keys from {}".format(cmd_args.destination_bucket))
-            dest_bucket_keys = get_s3_keys(s3_client, cmd_args.destination_bucket, cmd_args.prefix)
-            logger.debug("Keys from {} bucket:\n{}".format(cmd_args.destination_bucket, dest_bucket_keys))
             try:
                 deleted_keys = set(dest_bucket_keys) - set(source_bucket_keys)
                 tag_deleted_keys(s3_client, cmd_args.destination_bucket, deleted_keys)
             except:
                 logger.exception("")
-        sys.exit(127)
-
-        q = queue.Queue()
-        if len(diff_key) > 0:
-            logger.info("{0} objects not in destination bucket.\nAdding {0} keys to queue.".format(len(diff_key)))
-            for k in diff_key:
-                q.put(k)
-        else:
-            logger.info("No Objects to sync. Exiting!")
-            sys.exit(0)
-
-    ############################################################
-        for key in diff_key:
-            resource = session.resource('s3')
-            dest_bucket = resource.Bucket(cmd_args.destination_bucket)
-            dest_obj = dest_bucket.Object(key)
-
-            if not cmd_args.dry_run:
-                logger.info("Copying key: {}".format(key))
-                dest_obj.copy(
-                    {
-                        'Bucket': cmd_args.source_bucket,
-                        'Key': key
-                    }
-                )
-            else:
-                logger.debug("[DRY-RUN] Copying key: {}".format(key))
