96063a0bfdeb132fc89c4d9cd0985f5cb2719ea7
Daniel Thielking <daniel@thielking.cloud>
2018-04-26 20:44:13 +0200

    Added possibility to delete all versions in Bucket. Simplify API call.

--- s3_delete_keys.py
+++ s3_delete_keys.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--profile')
 parser.add_argument('-b', '--bucket')
 parser.add_argument('--delete', action='store_true')
+parser.add_argument('--delete-versions', action='store_true')
 parser.add_argument('-v', '--verbose', action='count')
 cmd_args = parser.parse_args()
 
@@ -19,34 +20,22 @@ PROFILE = cmd_args.profile
 BUCKET = cmd_args.bucket
 DELETE = cmd_args.delete
 VERBOSE = cmd_args.verbose
+DELETE_VERSIONS = cmd_args.delete_versions
 
 
-def get_s3_objects(session, bucket):
-    keys = list()
+def delete_s3_objects(session, bucket, with_versions=False):
     s3_resource = session.resource('s3')
     try:
-        for key in s3_resource.Bucket(bucket).objects.all():
-            keys.append(key.key)
+        if with_versions:
+            s3_resource.Bucket(bucket).objects.delete()
+            s3_resource.Bucket(bucket).objects_versions.delete()
+        else:
+            s3_resource.Bucket(bucket).objects.delete()
     except:
         logger.exception("")
-        sys.exit(127)
+        return False
     else:
-        return keys
-
-
-def delete_s3_objects(session, bucket, keys_to_delete):
-    s3_resource = session.resource('s3')
-    deleted = 0
-    while len(keys_to_delete):
-        key = keys_to_delete.pop()
-        try:
-            s3_resource.Object(bucket, key).delete()
-            deleted += 1
-        except:
-            logger.exception("")
-            keys_to_delete.append(key)
-
-    return deleted
+        return True
 
 
 if __name__ == '__main__':
@@ -58,18 +47,14 @@ if __name__ == '__main__':
     else:
         session = boto3.session.Session()
 
-    keys_to_delete = get_s3_objects(session, BUCKET)
-    logger.debug("Keys to be deleted: {}".format(keys_to_delete))
+    if DELETE and DELETE_VERSIONS:
+        answere = input("Are you sure you want to delete ALL keys and its versions in {}?[Y/N]: ".format(BUCKET))
+    elif DELETE:
+        answere = input("Are you sure you want to delete ALL keys in {}?[Y/N]: ".format(BUCKET))
 
-    if DELETE:
-        if keys_to_delete:
-            answere = input("Are you sure you want to delete ALL keys in {}?[Y/N]: ".format(BUCKET))
-            if answere.upper() == 'Y':
-                ret = delete_s3_objects(session, BUCKET, keys_to_delete)
-                print("Return: {}".format(ret))
-            else:
-                print("Aborting!")
-        else:
-            print("No keys in {}!".format(BUCKET))
+    if answere.upper() == 'Y':
+        ret = delete_s3_objects(session, BUCKET, with_versions=DELETE_VERSIONS)
+        if ret:
+            print("Deletion completed!")
     else:
-        logger.warning("[DRYRUN] following keys would be deleted.\n{}".format(keys_to_delete))
+        print("Aborting!")
