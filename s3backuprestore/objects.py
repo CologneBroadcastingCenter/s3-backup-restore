c998a92efca129da0f06f52cd5eeed0dfd0993dc
Daniel Thielking <daniel@thielking.cloud>
2018-06-15 13:57:20 +0200

    Added doc string. Added sending metrics how many objects are collected while iterating over the bucket.

--- s3backuprestore/objects.py
+++ s3backuprestore/objects.py
@@ -6,18 +6,22 @@ from random import shuffle
 
 from .config import Config
 from .log import logger
+from .cw import put_metric
 
 
-def get_objects(bucket, config=None, shuffle_list=True, objects_count=None):
+def get_objects(bucket, config=None, cw_metric_name=None, shuffle_list=True,
+                objects_count=None):
     """Returns objects from bucket and returns them as a list
 
     Args:
         bucket (string): S3 bucket.
-        config (Config, optional): Defaults to None.
-        Configuration object.
-        shuffle_list (boolean): Returnes a shuffled S3 keys list.
-        objects_count ([int], optional): Defaults to None.
-                                         Amount of keys to return.
+        config (Config, optional): Defaults to None. Configuration object.
+        cw_metric_name (string, optional): Defaults to None. Cloudwatch metric
+        name to publish to.
+        shuffle_list (boolean, optional): Defaults to True. Returnes a shuffled
+        S3 keys list.
+        objects_count (int, optional): Defaults to None. Amount of keys to
+        return.
 
     Returns:
         [list]: List of S3 keys.
@@ -26,6 +30,7 @@ def get_objects(bucket, config=None, shuffle_list=True, objects_count=None):
     logger.info("Receive objects from {}.".format(bucket))
     keys = list()
     start = time.time()
+    cw_metric_name = "ObjectsIn{}".format(bucket)
     try:
         if config:
             session = config.boto3_session()
@@ -36,15 +41,19 @@ def get_objects(bucket, config=None, shuffle_list=True, objects_count=None):
         sys.exit(127)
 
     try:
+        put_metric(cw_metric_name, 0, config=config)
         for key in session.resource('s3').Bucket(bucket).objects.all():
             keys.append(key.key)
 
             if time.time() - 30 > start:
                 logger.info("Received {} objects.".format(len(keys)))
                 start = time.time()
+                put_metric(cw_metric_name, len(keys), config=config)
             # Break condition to escape earlier thant complete bucket listing
             if objects_count and len(keys) >= objects_count:
                 break
+        else:
+            put_metric(cw_metric_name, len(keys), config=config)
 
         logger.info("Summary of received objects {}.".format(len(keys)))
     except:
